a906a8e76317d0dc2871e37481da61a45e67c59b
valed-dm <dmvaled@gmail.com>
2023-06-30 07:16:10 +0300

    src schemas added

---
+++ log_analyzer/src/schemas/__init__.py
@@ -0,0 +1,6 @@
+"""schemas module init"""
+
+from .cfg_data import ConfigDefault
+from .log_parser_data import ParserData
+from .log_report_data import Log, Report
+from .url_data import UrlGroupedData
