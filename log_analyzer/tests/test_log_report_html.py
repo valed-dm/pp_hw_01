5ca434f25a2442d2959e2dcdf6b8ba232c973672
valed-dm <dmvaled@gmail.com>
2023-07-05 12:42:14 +0300

    html test firefox replaced by brave
    to work with ubuntu

--- log_analyzer/tests/test_log_report_html.py
+++ log_analyzer/tests/test_log_report_html.py
@@ -8,15 +8,18 @@ from os.path import exists
 from pathlib import Path
 
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as BraveService
 # from selenium.webdriver.chrome.service import Service as ChromiumService
 from selenium.webdriver.common.by import By
-# from webdriver_manager.chrome import ChromeDriverManager
-# from webdriver_manager.core.utils import ChromeType
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
 
 from .t_reports.report_data import report_data
 from ..src.log_report import log_report
+
+
+# from selenium.webdriver.firefox.service import Service as FirefoxService
+# from webdriver_manager.firefox import GeckoDriverManager
 
 
 class TestLogReportData:
@@ -50,10 +53,17 @@ class TestlogReportHTML(unittest.TestCase, TestLogReportData):
         #         ).install()
         #     )
         # )
-        self.driver = webdriver.Firefox(
-            service=FirefoxService(
-                GeckoDriverManager()
-                .install()
+        # self.driver = webdriver.Firefox(
+        #     service=FirefoxService(
+        #         GeckoDriverManager()
+        #         .install()
+        #     )
+        # )
+        self.driver = webdriver.Chrome(
+            service=BraveService(
+                ChromeDriverManager(
+                    chrome_type=ChromeType.BRAVE
+                ).install()
             )
         )
 
