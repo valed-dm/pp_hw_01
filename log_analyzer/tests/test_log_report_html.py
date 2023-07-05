a681db5546d38175348cb6b69184da89a989d7dd
valed-dm <dmvaled@gmail.com>
2023-07-05 12:15:20 +0300

    html test chrome replaced by chromium
    to work with ubuntu

--- log_analyzer/tests/test_log_report_html.py
+++ log_analyzer/tests/test_log_report_html.py
@@ -8,7 +8,10 @@ from os.path import exists
 from pathlib import Path
 
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromiumService
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
 
 from .t_reports.report_data import report_data
 from ..src.log_report import log_report
@@ -37,7 +40,14 @@ class TestlogReportHTML(unittest.TestCase, TestLogReportData):
         self.report_abs_path = os.path.join(self.work_dir, self.report_rel_path)
         self.report_dir_abs_path = os.path.join(self.work_dir, self.report_dir)
         self.report_date = datetime.strptime("20230623", '%Y%m%d').date()
-        self.driver = webdriver.Chrome()
+        # self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(
+            service=ChromiumService(
+                ChromeDriverManager(
+                    chrome_type=ChromeType.CHROMIUM
+                ).install()
+            )
+        )
 
     def tearDown(self):
         """Clear selenium, delete t_report dir"""
