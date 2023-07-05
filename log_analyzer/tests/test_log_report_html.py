685c487076e4338a9583f50849e39c108b4841a9
valed-dm <dmvaled@gmail.com>
2023-07-05 13:22:41 +0300

    chrome_options --headless added

--- log_analyzer/tests/test_log_report_html.py
+++ log_analyzer/tests/test_log_report_html.py
@@ -8,6 +8,7 @@ from os.path import exists
 from pathlib import Path
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 # from selenium.webdriver.chrome.service import Service as BraveService
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
@@ -59,11 +60,16 @@ class TestlogReportHTML(unittest.TestCase, TestLogReportData):
         #         .install()
         #     )
         # )
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
         self.driver = webdriver.Chrome(
             service=ChromeService(
                 ChromeDriverManager()
                 .install()
-            )
+            ),
+            options=chrome_options
         )
 
     def tearDown(self):
